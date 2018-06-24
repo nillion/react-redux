d121cfb010917b3260b2b1372f16faf03e60d3f4
Arjun Sawhney <arjun7692@gmail.com>
2018-06-24 21:19:35 +0530

    refactor root to class based component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import YTSearch from 'youtube-api-search';
 import { SearchBar } from './components/searchbar';
 
 const API_KEY = 'AIzaSyA8fsMTdCl04gSl_ecbFcLHdY1bC7XELD8';
 
 /**
- * Root react application function
+ * Root react application
  */
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.initializeState();
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+
+  initializeState() {
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'Deep House' }, data => {
+      this.setState({ videos: data });
+      console.log('Fetch result', data);
+    });
+  }
 }
 
 /**
